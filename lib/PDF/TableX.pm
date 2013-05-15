70226dfe6572ffc6ff01b297fa57c9a1cec86b3c
Grzegorz Papkala <grzegorzpapkala@gmail.com>
2013-05-15 11:02:06 +0200

    Fixed POD on other modules

--- lib/PDF/TableX.pm
+++ lib/PDF/TableX.pm
@@ -13,7 +13,7 @@ use PDF::TableX::Cell;
 
 with 'PDF::TableX::Stylable';
 
-our $VERSION    = '0.01';
+our $VERSION    = '0.011';
 
 # public attrs
 has width         => (is => 'rw', isa => 'Num', default => 0);
--- lib/PDF/TableX/Column.pm
+++ lib/PDF/TableX/Column.pm
@@ -48,43 +48,33 @@ sub get_reg_width {
 1;
 
 =head1 NAME
+
 PDF::TableX::Column
 
 =head1 VERSION
+
 Version 0.01
-=cut
 
 =head1 SYNOPSIS
 
 =head1 FUNCTIONS
 
 =head1 AUTHOR
+
 Grzegorz Papkala, C<< <grzegorzpapkala at gmail.com> >>
 
 =head1 BUGS
-Please report any bugs or feature requests to C<bug-pdf-tablex at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=PDF-TableX>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+
+Please report any bugs or feature requests at: L<https://github.com/grzegorzpapkala/PDF-TableX/issues>
 
 =head1 SUPPORT
-You can find documentation for this module with the perldoc command.
-    perldoc PDF::TableX::Row
 
-You can also look for information at:
-=over 4
-=item * RT: CPAN's request tracker
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=PDF-TableX>
-=item * AnnoCPAN: Annotated CPAN documentation
-L<http://annocpan.org/dist/PDF-TableX>
-=item * CPAN Ratings
-L<http://cpanratings.perl.org/d/PDF-TableX>
-=item * Search CPAN
-L<http://search.cpan.org/dist/PDF-TableX/>
-=back
+PDF::TableX is hosted on GitHub L<https://github.com/grzegorzpapkala/PDF-TableX>
 
 =head1 ACKNOWLEDGEMENTS
 
 =head1 COPYRIGHT & LICENSE
+
 Copyright 2013 Grzegorz Papkala, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
